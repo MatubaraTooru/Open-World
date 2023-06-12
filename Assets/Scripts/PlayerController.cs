0cc6992cc0c4661d7d365a8d2c8fc1f2ebd1ea9d
MatubaraTooru <kurakura.ingen@gmail.com>
2023-06-12 20:53:20 +0900

    [update] アイテムショップとインベントリ完成

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,8 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody _rb;
     [SerializeField] float _moveSpeed = 5.0f;
-    [SerializeField] string _merchantTag;
-    [SerializeField] GameObject _shopPanel;
     float _h;
     float _v;
-    bool _shopping = false;
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
@@ -21,11 +18,6 @@ public class PlayerController : MonoBehaviour
     {
         _h = Input.GetAxisRaw("Horizontal");
         _v = Input.GetAxisRaw("Vertical");
-        
-        if (_shopping && Input.GetKeyDown(KeyCode.E))
-        {
-            _shopPanel.SetActive(true);
-        }
     }
     private void FixedUpdate()
     {
@@ -37,16 +29,8 @@ public class PlayerController : MonoBehaviour
         Vector3 dir = Vector3.forward * _v + Vector3.right * _h;
         dir.y = 0;
         if (dir != Vector3.zero) this.transform.forward = dir;
-        dir = dir.normalized * _moveSpeed;
         float y = _rb.velocity.y;
 
         _rb.velocity = dir * _moveSpeed + Vector3.up * y;
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag(_merchantTag))
-        {
-            _shopping = true;
-        }
     }
 }
